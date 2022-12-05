6a08d0276389e93ec02985e6e043994d3a0a015d
Arnab <arnabmondal55@gmail.com>
2022-12-05 12:50:31 +0530

    1st push

---
+++ src/Test/day1.java
@@ -0,0 +1,35 @@
+package Test;
+import org.testng.annotations.Test;
+
+public class day1{
+    @Test
+	public void Demo()
+	{
+		System.out.println("Arnab");
+		}
+	@Test
+	public void Demo1()
+	{
+		System.out.println("Piu");
+		}
+	}
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
---
+++ src/Test/day2.java
@@ -0,0 +1,11 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class day2 {
+	@Test
+ public void demo3() {
+	System.out.println("XXX");
+}
+
+}
